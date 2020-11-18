76538e17a2f67258af49239ec7d447044b7c3f8f
Wilfred <41619827+MrEquarell@users.noreply.github.com>
2020-11-18 13:09:27 +0100

    Add Form & luhn.js

--- js/exercice8.js
+++ js/exercice8.js
@@ -1,9 +1,20 @@
-let iimQuery = {
-    applyCss: function () {
-        customElements.forEach(function (element) {
-            console.log(element)
+let iimQueryCss = {
+    applyCss: function (elements, styles) {
+        elements.forEach(function (element,){
+            let el = document.getElementsById(element)
+
+            for (const property in styles) {
+                el.style[property] = styles[property]
+            }
         })
     }
+}
+
+let iimQuery = {
+    applyCSS: function(elements, styles) {
+        iimQueryCss.applyCss(elements, styles)
+    },
+    // other stuff
 }
 
 //target html tags with id
@@ -12,4 +23,7 @@ iimQuery.applyCss([
     'text-3',
     'text-4',
     'text-5',
-])
+], {
+    color: "red",
+    fontSize: "50px",
+})
---
+++ js/luhn.js
@@ -0,0 +1,24 @@
+
+
+const luhnNumber = (number) => {
+    number = String(number);
+
+    let sum = parseInt(number.charAt(number.length - 1));
+
+    for (let i = 0; i < number.length - 1; i++) {
+        let value = parseInt(number.charAt(i));
+
+        if (i % 2 === 0) {
+            value *= 2;
+        }
+
+        if (value > 9) {
+            value -= 9;
+        }
+
+        sum += value;
+    }
+
+    return sum % 10 === 0;
+}
+
