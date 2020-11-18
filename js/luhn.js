988a5b4dfdc6f3617272f121d9e1024b20c8c1c9
Wilfred <41619827+MrEquarell@users.noreply.github.com>
2020-11-18 14:15:35 +0100

    Update luhn.js

--- js/luhn.js
+++ js/luhn.js
@@ -1,24 +1,44 @@
-
-
-const luhnNumber = (number) => {
-    number = String(number);
-
-    let sum = parseInt(number.charAt(number.length - 1));
-
-    for (let i = 0; i < number.length - 1; i++) {
-        let value = parseInt(number.charAt(i));
-
-        if (i % 2 === 0) {
-            value *= 2;
+let Donation = {
+    payment: function(number) {
+        var creditCardIsValid = Payment.checkCreditCard(number)
+        if (creditCardIsValid) {
+            
         }
+    },
+    listPayments : function() {
 
-        if (value > 9) {
-            value -= 9;
-        }
-
-        sum += value;
     }
-
-    return sum % 10 === 0;
 }
+
+let Payment = {
+    checkCreditCard: function (number) {
+
+        number = String(number);
+
+        let sum = parseInt(number.charAt(number.length - 1));
+
+        for (let i = 0; i < number.length - 1; i++) {
+            let value = parseInt(number.charAt(i));
+
+            if (i % 2 === 0) {
+                value *= 2;
+            }
+
+            if (value > 9) {
+                value -= 9;
+            }
+
+            sum += value;
+        }
+
+        return sum % 10 === 0;
+    }
+}
+
+document.querySelector('.btn-event').addEventListener('click',function (e) {
+    Donation.payment(document.querySelector('.value-card').value)
+
+
+})
+
 
